5b97b0178a6ceb7d922ecd686853eea3a889f3d6
Silvia García Cacho <silvia@garca.info>
2019-01-24 09:34:41 +0100

    Solve list and card components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,30 +1,28 @@
 import React, {Component, Fragment} from "react";
 
 class CharacterCard extends Component {
+  houseOrNot(){
+    const {item} =this.props;
+    let house = "";
+    if(item.house !== ""){
+      house = item.house;
+      return house;  
+    } else{
+      house = "Do not belong to any house";
+      return house;
+    }
+  }
   render() {
     const {item} =this.props;
-    
     return (
       <Fragment>
-        {if(item.house !== ""){
-          return(
-          <li className="hp__list-item" key={item.id} id={item.id}>
+        <li className="hp__list-item" key={item.id} id={item.id}>
           <div className="hp__list-card">
             <img src={item.image} alt={item.name} className="list-card__image"></img>
             <h2 className="list-card__name">{item.name}</h2>
-            <p className="list-card__house">{item.house}</p>
+            <p className="list-card__house">{this.houseOrNot()}</p>
           </div>
-        </li>)  
-        } else{
-          return (
-          <li className="hp__list-item" key={item.id} id={item.id}>
-            <div className="hp__list-card">
-              <img src={item.image} alt={item.name} className="list-card__image"></img>
-              <h2 className="list-card__name">{item.name}</h2>
-              <p className="list-card__house">Do not belong to any house</p>
-            </div>
-          </li>)
-        } }
+        </li>
       </Fragment>
     );
   }
