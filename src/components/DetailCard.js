6a65e45309fd9b4453150ff273ffba3827c72115
Silvia García Cacho <silvia@garca.info>
2019-01-24 10:17:36 +0100

    Add dead or alive line and amend detail card view

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -1,6 +1,19 @@
 import React, {Component, Fragment} from "react";
 
 class DetailCard extends Component {
+
+  isCharacterAlive(){
+    const {selectedCharacter, characters} = this.props;
+    let aliveOrDead = "";
+    if(characters[selectedCharacter].alive === true){
+      aliveOrDead = "ALIVE"
+      return aliveOrDead;
+    } else {
+      aliveOrDead = "DEAD"
+      return aliveOrDead;
+    }
+  }
+
   isCharacterSelected(){
     const {selectedCharacter, characters} = this.props;
     if(selectedCharacter === ""){
@@ -8,14 +21,15 @@ class DetailCard extends Component {
     }else{
       return(<div className="hp__detail-card">
       <img src={characters[selectedCharacter].image} alt={characters[selectedCharacter].name}></img>
-      <h2 className="hp__detail-name">{characters[selectedCharacter].name}</h2>
-      <p className="hp__detail-house">{characters[selectedCharacter].house}</p>
-      <p className="hp__detail-born">{characters[selectedCharacter].yearOfBirth}</p>
-      <p className="hp__detail-patronus">{characters[selectedCharacter].patronus}</p>
-      <p className="hp__detail-status"></p>
+      <h2 className="hp__detail-name">Name: {characters[selectedCharacter].name}</h2>
+      <p className="hp__detail-house">House: {characters[selectedCharacter].house}</p>
+      <p className="hp__detail-born">Born: {characters[selectedCharacter].yearOfBirth}</p>
+      <p className="hp__detail-patronus">Patronus: {characters[selectedCharacter].patronus}</p>
+      <p className="hp__detail-status">State: {this.isCharacterAlive()}</p>
     </div>)
     }
   }
+  
   render() {
     return (
       <Fragment>
