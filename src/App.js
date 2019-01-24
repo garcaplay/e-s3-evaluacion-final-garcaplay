35eac0d0197560d5942640e8d257071478f3ac69
Silvia García Cacho <silvia@garca.info>
2019-01-24 08:28:55 +0100

    Add basic html structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-      
+        <div className="hp__page">
+        <header className="hp__header">
+          <h2 className="hp__header-title">Harry Potter Characters</h2>
+        </header>
+        <main className="hp__main">
+          <div className="hp__filter">
+            <input type="text" className="hp__filter-input" placeholder="Who are you looking for?"></input>
+          </div>
+          <div className="hp__container">
+            <ul className="hp__list">
+              <li className="hp__list-item">
+                <div className="hp__list-card">
+                  <img src="" alt="" className="list-card__image"></img>
+                  <h2 className="list-card__name"></h2>
+                  <p className="list-card__house"></p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </main>
+        </div>
       </div>
     );
   }
