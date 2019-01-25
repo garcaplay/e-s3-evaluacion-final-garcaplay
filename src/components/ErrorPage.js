bdf75640958ef1350379a3e74cddd27d0ccaf664
Silvia García Cacho <silvia@garca.info>
2019-01-25 10:05:29 +0100

    Add button back from error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,10 +1,14 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class ErrorPage extends Component {
   render() {
     return (
       <div className="errorpage">
         <p>No hay datos</p>
+        <Link to="/" style={{textDecoration:"none", color:"inherit"}}>
+              <div className="hp-detail-link">Go back</div>
+              </Link>
       </div>  
     );
   }
